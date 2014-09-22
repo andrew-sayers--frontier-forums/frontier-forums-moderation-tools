1ab23f0d4780593b114c9b027c992d95ca16ea00
Andrew Sayers <frontier-forums@pileofstuff.org>
2014-09-22 14:13:25 +0100

    Add "production" environment

--- Firefox/lib/settings.js
+++ Firefox/lib/settings.js
@@ -1,3 +1,3 @@
 exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*"];
 exports.contentScriptWhen = "end";
-exports.contentScriptFile = ["self.data.url('BabelExt.js')","self.data.url('jquery.min.js')","self.data.url('extension.js')"];
+exports.contentScriptFile = ["BabelExt.js","jquery.min.js","extension.js"];
