cdcdbddc818652f1b3050d17a54443827d019979
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-05 10:12:30 +0000

    Updated version number to 4.0
    Actual command: ./release.sh 4.0

--- Firefox/lib/settings.js
+++ Firefox/lib/settings.js
@@ -1,3 +1,3 @@
 exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*"];
 exports.contentScriptWhen = "start";
-exports.contentScriptFile = ["BabelExt.js","jquery-2.1.1.min.js","selectionchange.js","extension.js"];
+exports.contentScriptFile = ["BabelExt.js","jquery-2.1.1.min.js","selectionchange.js","bulletin_board_api.js","dashboard.js","extension.js"];
