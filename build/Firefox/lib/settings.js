8ca9b72d828241688cf3de549d44423ebb4277a2
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-16 02:49:05 +0000

    Updated version number to 4.2
    Actual command: ./release.sh 4.2

--- build/Firefox/lib/settings.js
+++ build/Firefox/lib/settings.js
@@ -1,3 +1,4 @@
-exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*"];
+exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*","http://forumstest.frontier.co.uk/*","https://forumstest.frontier.co.uk/*"];
 exports.contentScriptWhen = "start";
-exports.contentScriptFile = ["BabelExt.js","jquery-2.1.1.min.js","selectionchange.js","bulletin_board_api.js","dashboard.js","extension.js"];
+exports.contentScriptFile = ["lib/BabelExt.js","lib/jquery-2.1.1.min.js","lib/selectionchange.js","src/bulletin_board_api.js","src/cacheable.js","src/variables.js","src/dashboard.js","src/legacy.js","src/main.js"];
+exports.contentStyleFile = ["src/main.css"];
