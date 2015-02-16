85075c711a11ccdf599559de92dac5e7496b046e
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-16 02:02:28 +0000

    Add documentation

--- script/jsdoc.sh
+++ script/jsdoc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm -rf doc
 ./jsdoc-3.2.2/jsdoc -d doc -c jsdoc-conf.json -t templates/jaguarjs-jsdoc-master $( sed -n '/contentScriptFiles/ { s/^.*\[ *"*\|"* *\].*$\|", "/ /g ; p }' conf/settings.json )
