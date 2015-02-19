8cd402911be208ccb3a249fe15b5b423eeeb2a7d
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-19 07:22:22 +0000

    Improvements for jsdoc.sh

--- script/jsdoc.sh
+++ script/jsdoc.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf doc
-./jsdoc/jsdoc -d doc -c jsdoc-conf.json -t templates/jaguarjs $( sed -n '/contentScriptFiles/ { s/^.*\[ *"*\|"* *\].*$\|", "/ /g ; p }' conf/settings.json )
+./jsdoc/jsdoc -d doc -c jsdoc-conf.json -t templates/jaguarjs $( echo "console.log($(< conf/settings.json ).contentScriptFiles.join(' '));" | nodejs )
+
+sed -i -e 's/[ \t]*$//' doc/*.html
