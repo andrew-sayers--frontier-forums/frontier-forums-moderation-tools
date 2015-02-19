828ab35223a36fd2fa5ec24f7ec1806d04b751af
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-02-19 07:39:35 +0000

    Updated version number to 4.4
    Actual command: ./script/release.sh 4.4

--- build/Firefox/lib/settings.js
+++ build/Firefox/lib/settings.js
@@ -1,4 +1,4 @@
 exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*","http://forumstest.frontier.co.uk/*","https://forumstest.frontier.co.uk/*"];
 exports.contentScriptWhen = "start";
-exports.contentScriptFile = ["lib/BabelExt.js","lib/jquery-2.1.1.min.js","lib/selectionchange.js","src/bulletin_board_api.js","src/cacheable.js","src/variables.js","src/dashboard.js","src/legacy.js","src/main.js"];
+exports.contentScriptFile = ["lib/BabelExt.js","lib/BabelExtResources.js","lib/jquery-2.1.1.min.js","lib/selectionchange.js","src/bulletin_board_api.js","src/cacheable.js","src/variables.js","src/violations.js","src/dashboard.js","src/report.js","src/legacy.js","src/main.js"];
 exports.contentStyleFile = ["src/main.css"];
