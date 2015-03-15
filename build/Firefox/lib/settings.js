0c222d4b177633ec84864f10d323670e55b9c9d8
Andrew Sayers <frontier-forums@pileofstuff.org>
2015-03-15 11:29:21 +0000

    Updated version number to 4.6
    Actual command: ./script/release.sh 4.6

--- build/Firefox/lib/settings.js
+++ build/Firefox/lib/settings.js
@@ -1,4 +1,4 @@
 exports.include = ["http://forums.frontier.co.uk/*","https://forums.frontier.co.uk/*","http://forumstest.frontier.co.uk/*","https://forumstest.frontier.co.uk/*"];
 exports.contentScriptWhen = "start";
-exports.contentScriptFile = ["lib/BabelExt.js","lib/BabelExtResources.js","lib/jquery-2.1.1.min.js","lib/selectionchange.js","src/bulletin_board_api.js","src/cacheable.js","src/widgets/severity_slider.js","src/variables.js","src/violations.js","src/dashboard.js","src/report.js","src/legacy.js","src/main.js"];
+exports.contentScriptFile = ["lib/BabelExt.js","lib/BabelExtResources.js","lib/jquery-2.1.1.min.js","lib/selectionchange.js","lib/Chart.js","src/debug_log.js","src/bulletin_board_api.js","src/cacheable.js","src/widgets/severity_slider.js","src/variables.js","src/violations.js","src/dashboard.js","src/report.js","src/form_keys.js","src/legacy.js","src/main.js"];
 exports.contentStyleFile = ["src/widgets/severity_slider.css","src/main.css"];
